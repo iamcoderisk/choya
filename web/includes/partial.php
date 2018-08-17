da20da00ce10c7faaac841fd647c236ac75c2656
Clinton Nzedimma <34009341+clintonnzedimma@users.noreply.github.com>
2018-08-17 23:06:55 +0100

    Add files via upload

---
+++ web/includes/partial.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo $title; ?></title>
+	<link rel="stylesheet" type="text/css" href="../../web/assets/css/main.css">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">	
+</head>
